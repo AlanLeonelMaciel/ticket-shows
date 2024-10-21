58fa942240dfcfcd2d40b57c78ead99194431272
AlanLeonelMaciel <alanleonelmaciel@gmail.com>
2024-10-21 16:05:08 -0300

    profile edit y update listos

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -41,27 +41,57 @@ class Profile extends CI_Controller {
         $data = [
             'profile' => $profile,
             'zones' => $this->Zone_model->get_all_zones(),
-            'address' => $this->Address_model->get_address_by_id($profile->address_id)
+            'address' => $this->Address_model->get_address_by_id($profile->address_id),
+            'inner_view_path' => 'profiles/edit'
         ];
-        $this->load->view('profiles/edit', $data);
+        $this->load->view('layouts/main', $data);
     }
 
     public function update()
     {
-        // Obtener el user_id del usuario autenticado
+        // Obtener el user_id y el profile_id del usuario autenticado
         $user_id = $this->session->userdata('user_id');
+        $profile_id = $this->session->userdata('profile_id');
         
-        if (empty($user_id)) {
-            show_404();
+        if(empty($user_id) || empty($profile_id)) {
+            // Redirige al inicio de sesión o muestra un error
+            redirect('auth/login_form');
         }
 
-        $this->form_validation->set_rules('name', 'Name', 'required');
-        $this->form_validation->set_rules('surname', 'Surname', 'required');
-        $this->form_validation->set_rules('phone', 'Phone', 'required');
-        $this->form_validation->set_rules('dni', 'DNI', 'required');
-        $this->form_validation->set_rules('street', 'Street', 'required');
-        $this->form_validation->set_rules('number', 'Number', 'required');
-        $this->form_validation->set_rules('zone_id', 'Zone', 'required');
+        $this->form_validation->set_rules(
+            'name', 'Name', 
+            'required|alpha|min_length[2]|max_length[100]'
+        );
+
+        $this->form_validation->set_rules(
+            'surname', 'Surname', 
+            'required|alpha|min_length[2]|max_length[100]'
+        );
+
+        $this->form_validation->set_rules(
+            'phone', 'Phone', 
+            'required|numeric|min_length[6]|max_length[15]'
+        );
+
+        $this->form_validation->set_rules(
+            'dni', 'DNI', 
+            'required|numeric|min_length[7]|max_length[8]'
+        );
+
+        $this->form_validation->set_rules(
+            'street', 'Street', 
+            'required|alpha_numeric_spaces|min_length[3]|max_length[100]'
+        );
+
+        $this->form_validation->set_rules(
+            'number', 'Number', 
+            'required|numeric'
+        );
+
+        $this->form_validation->set_rules(
+            'zone_id', 'Zone', 
+            'required|integer'
+        );
 
         $input_data = [
             'name' => $this->input->post('name'),
@@ -74,24 +104,20 @@ class Profile extends CI_Controller {
             'zone_id' => $this->input->post('zone_id')
         ];
 
-        if ($this->form_validation->run() === FALSE) {
+        if($this->form_validation->run() === FALSE) {
             $this->session->set_flashdata('input_data', $input_data);
             $this->session->set_flashdata('errors', $this->form_validation->error_array());
             redirect('profile/edit');
-        } else {
-            $profile_data = [
-                'name' => $this->input->post('name'),
-                'surname' => $this->input->post('surname'),
-                'phone' => $this->input->post('phone'),
-                'dni' => $this->input->post('dni'),
-                'picture' => $this->input->post('picture'),
-                'address_id' => $this->Address_model->get_address_id($input_data['street'], $input_data['number'], $input_data['zone_id']) 
-                                ?: $this->Address_model->create_address($input_data['street'], $input_data['number'], $input_data['zone_id'])
-            ];
-
-            // Mostrar el array profile_data
-            print_r($profile_data);
-            $this->Profile_model->update_profile($user_id, $profile_data);
+        }else{
+            // Obtener el id de la dirección si no existe crearla
+            $address_id = $this->Address_model->get_address_id($input_data['street'], $input_data['number'], $input_data['zone_id']) 
+                          ?: $this->Address_model->create_address($input_data['street'], $input_data['number'], $input_data['zone_id']);
+            // Eliminar los campos de dirección del array $input_data
+            unset($input_data['street'], $input_data['number'], $input_data['zone_id']);
+            // Combinar los arrays $input_data y $profile_data en un solo array.
+            $profile_data = array_merge($input_data, ['address_id' => $address_id]);
+            // Actualizar el perfil
+            $this->Profile_model->update_profile($profile_id, $profile_data);
             redirect('profile/edit'); // Redirigir a la misma página de edición después de guardar los cambios
         }
     }
--- application/views/profiles/edit.php
+++ application/views/profiles/edit.php
@@ -1,55 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Edit Profile</title>
-</head>
-<body>
-    <h1>Edit Profile</h1>
-    <?php if ($this->session->flashdata('errors')): ?>
-        <div style="color: red;">
-            <?php foreach ($this->session->flashdata('errors') as $error): ?>
-                <p><?php echo $error; ?></p>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
+<h1>Edit Profile</h1>
+<?php if($this->session->flashdata('errors')): ?>
+    <div style="color: red;">
+        <?php foreach($this->session->flashdata('errors') as $error): ?>
+            <p><?php echo $error; ?></p>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
 
-    <?php
-    $input_data = $this->session->flashdata('input_data');
-    ?>
+<?php
+$input_data = $this->session->flashdata('input_data');
+?>
 
-    <?php echo form_open('profile/update'); ?>
-        <label for="name">Name</label>
-        <input type="text" name="name" value="<?php echo set_value('name', isset($input_data['name']) ? $input_data['name'] : $profile->name); ?>"><br>
-                
-        <label for="surname">Surname</label>
-        <input type="text" name="surname" value="<?php echo set_value('surname', isset($input_data['surname']) ? $input_data['surname'] : $profile->surname); ?>"><br>
+<?php echo form_open('profile/update'); ?>
+<label for="name">Name</label>
+<input type="text" name="name" value="<?php echo isset($input_data['name']) ? $input_data['name'] : (isset($profile->name) ? $profile->name : ''); ?>"><br>
 
-        <label for="phone">Phone</label>
-        <input type="text" name="phone" value="<?php echo set_value('phone', isset($input_data['phone']) ? $input_data['phone'] : $profile->phone); ?>"><br>
+<label for="surname">Surname</label>
+<input type="text" name="surname" value="<?php echo isset($input_data['surname']) ? $input_data['surname'] : (isset($profile->surname) ? $profile->surname : ''); ?>"><br>
 
-        <label for="dni">DNI</label>
-        <input type="text" name="dni" value="<?php echo set_value('dni', isset($input_data['dni']) ? $input_data['dni'] : $profile->dni); ?>"><br>
+<label for="phone">Phone</label>
+<input type="text" name="phone" value="<?php echo isset($input_data['phone']) ? $input_data['phone'] : (isset($profile->phone) ? $profile->phone : ''); ?>"><br>
 
-        <label for="picture">Picture</label>
-        <input type="text" name="picture" value="<?php echo set_value('picture', isset($input_data['picture']) ? $input_data['picture'] : $profile->picture); ?>"><br>
+<label for="dni">DNI</label>
+<input type="text" name="dni" value="<?php echo isset($input_data['dni']) ? $input_data['dni'] : (isset($profile->dni) ? $profile->dni : ''); ?>"><br>
 
-        <label for="street">Street</label>
-        <input type="text" name="street" value="<?php echo set_value('street', isset($input_data['street']) ? $input_data['street'] : ($address ? $address->street : '')); ?>"><br>
+<label for="picture">Picture</label>
+<input type="text" name="picture" value="<?php echo isset($input_data['picture']) ? $input_data['picture'] : (isset($profile->picture) ? $profile->picture : ''); ?>"><br>
 
-        <label for="number">Number</label>
-        <input type="text" name="number" value="<?php echo set_value('number', isset($input_data['number']) ? $input_data['number'] : ($address ? $address->number : '')); ?>"><br>
-        
-        <label for="zone_id">Zone</label>
-        <select name="zone_id">
-            <option value="">Seleccione una zona</option> <!-- Opción por defecto vacía -->
-            <?php foreach ($zones as $zone): ?>
-                <option value="<?php echo $zone->id; ?>" <?php echo set_select('zone_id', $zone->id, isset($input_data['zone_id']) ? $input_data['zone_id'] == $zone->id : ($address && $address->zone_id == $zone->id)); ?>>
-                    <?php echo $zone->name; ?>
-                </option>
-            <?php endforeach; ?>
-        </select><br>
+<label for="street">Street</label>
+<input type="text" name="street" value="<?php echo isset($input_data['street']) ? $input_data['street'] : (isset($address->street) ? $address->street : ''); ?>"><br>
 
-        <input type="submit" name="submit" value="Update Profile">
-    <?php echo form_close(); ?>
-</body>
-</html>
+<label for="number">Number</label>
+<input type="text" name="number" value="<?php echo isset($input_data['number']) ? $input_data['number'] : (isset($address->number) ? $address->number : ''); ?>"><br>
+
+<label for="zone_id">Zone</label>
+<select name="zone_id">
+    <option value="">Seleccione una zona</option> <!-- Opción por defecto vacía -->
+    <?php foreach ($zones as $zone): ?>
+        <option value="<?php echo $zone->id; ?>" <?php echo isset($input_data['zone_id']) ? ($input_data['zone_id'] == $zone->id ? 'selected' : '') : (isset($address->zone_id) && $address->zone_id == $zone->id ? 'selected' : ''); ?>>
+            <?php echo $zone->name; ?>
+        </option>
+    <?php endforeach; ?>
+</select><br>
+
+<input type="submit" name="submit" value="Update Profile">
+<?php echo form_close(); ?>
