469707487dcd8bb555ae63587f64e5fad760e874
Rodrigo <rfnobando@gmail.com>
2024-10-23 00:32:21 -0300

    bootstrap/bootswatch darkly theme añadido

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
 </head>
 <body>
     <?php $this->load->view($inner_view_path); ?> 
+    <script src="<?php echo base_url('assets/js/bootstrap.bundle.js'); ?>"></script>
 </body>
 </html>
